d2a3b7252a3a717457eebd17db14e227951206ee
raza-aidev <raza.aidev@gmail.com>
2026-06-28 19:04:36 +0530

    notes and examples of python membership operator

---
+++ phase1/python-membership-opr.py
@@ -0,0 +1,23 @@
+mainString = "LearningML"
+a = "ea"
+b = 'learn'
+c = 'ML'
+
+#----------------------------------------
+# Use of 'in' membership operator
+
+print(a, "in", mainString, ": ", a in mainString)
+print(b, "in", mainString, ": ", b in mainString)
+print(c, "in", mainString, ": ", c in mainString)
+
+# there are 2 types of membership operators "in" and "not in"
+# membership operator checks whether the sub string is present in main string
+# or you can say that an item is a part list or tuple or a part of any sub-list or sub-tuple of list or tuple included in.
+
+
+#----------------------------------------
+# Use of 'not in' membership operator
+
+print(a, "not in", mainString, ": ", a not in mainString)
+print(b, "not in", mainString, ": ", b not in mainString)
+print(c, "not in", mainString, ": ", c not in mainString)
