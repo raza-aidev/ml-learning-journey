e9d187c9f92d752997330498ca284041a3f06a3f
raza-aidev <raza.aidev@gmail.com>
2026-06-29 22:29:28 +0530

    2 examples of Walrus operator

--- phase1/python-walrus-opr.py
+++ phase1/python-walrus-opr.py
@@ -16,3 +16,36 @@ In Short: Assign first, then immediately use the assigned value.
 
 it is also called as assignment expression operator.
 """
+
+
+
+print("-------Example 1-------")
+
+"""
+replicate:
+age = int(input("Enter Age: "))
+
+if (age >= 18):
+    print(f"This person is adult, Age: {age}")
+"""
+
+if ((age := int(input("Enter Age: ")))>=18):
+    print(f"This person is adult, Age: {age}")
+else:
+    print(f"This person is not an Adult. Age: {age}") 
+
+
+"""
+replicate:
+
+line = input("Enter text: ")
+
+while(line != 'quit'):
+    print(line)
+    line = input("Enter text: ")
+"""
+print("------Example 2-------")
+
+while ((line := input("Enter text: ")) != 'quit'):
+    print(line)
+
