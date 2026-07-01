9811b3dbf1d9163c03edd4fa32f7b8156ebbd50a
raza-aidev <raza.aidev@gmail.com>
2026-07-01 07:38:47 +0530

    Additonal examples of if and conditiona expression

--- phase1/python-if-conditional.py
+++ phase1/python-if-conditional.py
@@ -32,14 +32,38 @@ Age 5 to 17: $10
 Age 18 to 64: $15
 Age 65 or older: $12
 """
-if((Age := int(input("Enter the valid Age: "))) < 5 ):
-    if(Age <= 0):
-        print("Not A Valid Age!")
-    else:
-        print("It's FREE!")
-elif((Age >= 5) and (Age <= 17)):
-    print("Please pay $10 per ticket.")
-elif((Age >= 18) and (Age <= 64)):
-    print("Please Pay $15.")
-else:
-    print("You ll have to pay $12.")
+# if((Age := int(input("Enter the valid Age: "))) < 5 ):
+#     if(Age <= 0):
+#         print("Not A Valid Age!")
+#     else:
+#         print("It's FREE!")
+# elif((Age >= 5) and (Age <= 17)):
+#     print("Please pay $10 per ticket.")
+# elif((Age >= 18) and (Age <= 64)):
+#     print("Please Pay $15.")
+# else:
+#     print("You ll have to pay $12.")
+
+
+#-------------Example 4 -----------------
+
+
+"""  Write a program to check if a given year is a leap year. A year is a leap year if it is divisible by 4, 
+except for end-of-century years (ending in 00), which must also be divisible by 400."""
+
+# year = int(input("Enter the year: "))
+
+# if(((year%4) == 0) or (((year%400)==0)) and ((year%100) != 0)):
+#     print("Year is a leap year.")
+# else:
+#     print("Year is not a leap year.")
+
+#-------------example 5-----------------
+""" oneliner conditional Operation or ternary operator"""
+
+# age = int(input("ENter Age: "))
+
+# status = "Adult" if age >= 18 else "minor"
+
+# print(status)
+
