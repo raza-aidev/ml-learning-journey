4673dd3105bb338ca4a91b1d03015eb52e46c68a
raza-aidev <raza.aidev@gmail.com>
2026-07-04 20:23:31 +0530

    added Star Patterens examples

---
+++ phase1/for-loop.py
@@ -0,0 +1,28 @@
+## simple for loop
+# for i in range(0, 20):
+#     print(i);
+
+# # right angle pyramid
+
+# for i in range (1, n := int(input("Enter the height of the pyramid: "))):
+#     print("*"*i)
+
+# Square of stars
+# n = int(input("Enter the size of square: "))
+# for i in range(n):
+#     for j in range(n):
+#         print(f"*", end=" ")  #replace string to print with the expected string
+#     print()
+
+#increasing triangle 
+# n = int(input("Enter the range: "))
+
+# for i in range(1, n+1):
+#     print("*"*i)
+
+#decreasing triangle
+
+# n = int(input("Enter the size of the triangle: "))
+
+# for i in range(n,0, -1):
+#     print("*"*i)
