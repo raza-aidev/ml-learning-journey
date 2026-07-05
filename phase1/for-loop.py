38d8e502d11128834e8063341c34a2fc2b14c347
raza-aidev <raza.aidev@gmail.com>
2026-07-05 13:52:07 +0530

    Adding more pyramid patterns

--- phase1/for-loop.py
+++ phase1/for-loop.py
@@ -44,7 +44,48 @@
 """
 n = int(input("Enter the size of the triangle: "))
 
+# for i in range(n):
+#     for j in range(i,n):
+#         print(" ", end="")
+#     print("*"*(i+1))
+
+# for i in range(n):
+#     for j in range(i, n):
+#         print(" ", end=" ")
+#     for k in range(i+1):
+#         print("*", end=" ")
+#     for l in range(i):
+#         print("*", end=" ")
+#     print()
+
+# for i in range(n):
+#     for j in range(i):  # to print empty right angle triangle
+#         print(" ", end=" ")
+#     for k in range(i, n):  # To print inverse right angle with stars
+#         print("*", end=" ")
+#     for l in range(n,i+1,-1):  #To print 3rd inverse right angle triangle
+#         print("*", end=" ")
+#     for m in range(i):   # to print empty right angle triangle
+#         print(" ", end=" ")
+#     print()
+
 for i in range(n):
-    for j in range(i,n):
+    for j in range(n, i, -1):
+        print(" ", end =" ")
+    for k in range(i):
+        print("*", end=" ")
+    for l in range(i+1):
+        print("*", end=" ")
+    for m in range(n,i,-1):
         print(" ", end="")
-    print("*"*(i+1))
+    print()
+for i in range(n):
+    for j in range(i):
+        print(" ", end=" ");
+    for k in range(n,i, -1):
+        print("*", end=" ")
+    for l in range(n+1,i,-1):
+        print("*", end=" ")
+    for m in range(i):
+        print(" ", end=" ")
+    print()
