4e643750e4f85210a0cb51dbfa9824c8abdef532
raza-aidev <raza.aidev@gmail.com>
2026-07-06 23:25:19 +0530

    Adding function example

---
+++ phase1/functions.py
@@ -0,0 +1,27 @@
+# def greet(name):
+#     print(f"Welcome, {name}")
+
+
+# greet("Abdul")\
+
+# def add(a,b,c):
+#     result = a + b + c
+#     return result
+
+# print(add(10,23,10))
+
+# def return_multiple():
+#     a = 10; b = 12; c=13
+#     return a,b,c
+
+# x,y,z = return_multiple()
+
+# print("x: ", x, "y: ",y, "z: ", z)
+
+def return_multiple():
+    a = 10; b = 12; c=13
+    return a,b,c
+tuple1 = ()
+tuple1 = return_multiple()
+
+print("Tuple Value: ", tuple1)
