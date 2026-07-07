d78f01a2b42d33caf6fdb9654e16e3aa1a3991da
raza-aidev <raza.aidev@gmail.com>
2026-07-07 23:18:45 +0530

    Added function arguments example. Positional type, keyword type, only positionl type, default type

--- phase1/functions.py
+++ phase1/functions.py
@@ -18,10 +18,34 @@
 
 # print("x: ", x, "y: ",y, "z: ", z)
 
-def return_multiple():
-    a = 10; b = 12; c=13
-    return a,b,c
-tuple1 = ()
-tuple1 = return_multiple()
+# def return_multiple():
+#     a = 10; b = 12; c=13
+#     return a,b,c
+# tuple1 = ()
+# tuple1 = return_multiple()
 
-print("Tuple Value: ", tuple1)
+# print("Tuple Value: ", tuple1)
+
+#-------------types of function arguments------------
+
+def addition(a,b): 
+    """Positional_arguments"""
+    result = a + b
+    return result
+
+print(addition(12, 13))
+
+def mens_bio(age, name):
+    """ keyword type aruments """
+    return f"name is: {name} and Age is: {age}"
+
+print(mens_bio(name="Kartik", age="28"))
+
+def multiplication(a, b,/):
+    """ positional-only type"""
+    return a*b
+
+print("Result of multiplication is: ", multiplication(12,2))
+
+
+
