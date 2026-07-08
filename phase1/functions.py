4b0eba03a1a38d69b3ca4ec35d9098ff5fe9da2d
raza-aidev <raza.aidev@gmail.com>
2026-07-08 23:39:10 +0530

    added arbitrary argument of functions

--- phase1/functions.py
+++ phase1/functions.py
@@ -28,29 +28,49 @@
 
 #-------------types of function arguments------------
 
-def addition(a,b): 
-    """Positional_arguments"""
-    result = a + b
-    return result
+# def addition(a,b): 
+#     """Positional_arguments"""
+#     result = a + b
+#     return result
 
-print(addition(12, 13))
+# print(addition(12, 13))
 
-def mens_bio(age, name):
-    """ keyword type aruments """
-    return f"name is: {name} and Age is: {age}"
+# def mens_bio(age, name):
+#     """ keyword type aruments """
+#     return f"name is: {name} and Age is: {age}"
 
-print(mens_bio(name="Kartik", age="28"))
+# print(mens_bio(name="Kartik", age="28"))
 
-def multiplication(a, b,/):
-    """ positional-only type"""
-    return a*b
+# def multiplication(a, b,/):
+#     """ positional-only type"""
+#     return a*b
 
-print("Result of multiplication is: ", multiplication(12,2))
+# print("Result of multiplication is: ", multiplication(12,2))
 
-def area_of_rectangle(*, length, breadth, height):
-    """ keyword-type only """
-    return length*breadth*height
+# def area_of_rectangle(*, length, breadth, height):
+#     """ keyword-type only """
+#     return length*breadth*height
 
-print("Area of a rectangle: ", area_of_rectangle(length=10, breadth=9, height=12))
+# print("Area of a rectangle: ", area_of_rectangle(length=10, breadth=9, height=12))
 
 
+# def add(*nums):
+#     """ arbitrary Arguments"""
+#     result = 0
+#     for num in nums:
+#         result += num
+    
+#     return result
+
+# print("Result of adding", add(21,12,15,17,10))
+
+def get_total_marks_of_student(name, *marks):
+    """ Example of arbitrarry arguments """
+    total = 0
+    for mark in marks:
+        total += mark
+    
+    return f"Total of {name} is: {total}"
+
+# print(get_total_marks_of_student("Harish", 12, 13, 15,16))
+# print(get_total_marks_of_student("Namita", 11, 9, 12.5, 8))
