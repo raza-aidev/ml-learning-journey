709b8a7a034b2817643b87d20aa31ec41d072740
raza-aidev <raza.aidev@gmail.com>
2026-07-08 23:59:29 +0530

    Added key word argument - Type of Arbitrary Arguments

--- phase1/functions.py
+++ phase1/functions.py
@@ -47,20 +47,20 @@
 
 # print("Result of multiplication is: ", multiplication(12,2))
 
-# def area_of_rectangle(*, length, breadth, height):
-#     """ keyword-type only """
-#     return length*breadth*height
+def area_of_rectangle(*, length, breadth, height):
+    """ keyword-type only """
+    return f"Area of a rectangle: {length*breadth*height}"
 
 # print("Area of a rectangle: ", area_of_rectangle(length=10, breadth=9, height=12))
 
 
-# def add(*nums):
-#     """ arbitrary Arguments"""
-#     result = 0
-#     for num in nums:
-#         result += num
+def add(*nums):
+    """ arbitrary Arguments"""
+    result = 0
+    for num in nums:
+        result += num
     
-#     return result
+    return f"Result of addition is: {result}"
 
 # print("Result of adding", add(21,12,15,17,10))
 
@@ -74,3 +74,9 @@ def get_total_marks_of_student(name, *marks):
 
 # print(get_total_marks_of_student("Harish", 12, 13, 15,16))
 # print(get_total_marks_of_student("Namita", 11, 9, 12.5, 8))
+
+def generate_dic_for(**details):
+    return f"The dictionary of requested details: {details}"
+
+print(generate_dic_for(name="Abdul Razzak", age=27, city="Pune"))
+print(generate_dic_for(Object="Smart Phone", Brand="Apple", Model="I-Phone 17 Pro", Storage="1TB"))
