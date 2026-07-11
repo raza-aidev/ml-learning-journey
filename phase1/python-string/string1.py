0f977278c1ad5d5388a371df42d3a71685585b99
raza-aidev <raza.aidev@gmail.com>
2026-07-11 09:02:16 +0530

    Adding String examples

---
+++ phase1/python-string/string1.py
@@ -0,0 +1,17 @@
+str1 = "String"
+str2 = 'In Python'
+str3 = ''' Hello world!
+I'm learning python '''
+str4 = """Let's Start"""   # these are the definations of String in Python
+
+print(f"Accessing Python String str1[1]: ", str1[1])
+
+print("Slicing the string: str2[:3]: ", str2[:3])
+print("Slicing str2[5:8]: ", str2[5:8])
+print("Slicing str3[7:0]: ", str3[7:0])
+
+print("Slicing str3[-1:4]: ", str3[-1:4])
+
+print("Slicing str3[:6][:2]: ", str3[:6][:2])
+print("slicing str3[:6][-3:-1]", str3[:6][-3:-1])
+ 
