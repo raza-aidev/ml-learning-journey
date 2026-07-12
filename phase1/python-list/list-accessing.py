69601324f5ba548e4853e1ae0028502d180eff33
raza-aidev <raza.aidev@gmail.com>
2026-07-12 18:07:45 +0530

    Adding more examples of list accesing

--- phase1/python-list/list-accessing.py
+++ phase1/python-list/list-accessing.py
@@ -5,3 +5,15 @@ print("accesing every thrid element of a list: ", list1[::3])
 print(f"Origination list is {list1}")
 print(f"reversed list is {list1[::-1]}")
 print(f"reversed pf the string {str1} is {str1[::-1]}")
+
+print(f"Let's check original list after revesal: {list1}")
+
+
+print(f"Accessing indivisual values list1[1]: {list1[1]}")
+print(f"negative value accessing: {list1[-4]}")
+print(f"Accessing each element using for loop: ")
+for items in list1:
+    print(f"{items}")
+
+for i in range(len(list1)):
+    print(f"Index: {i}, Value: {list1[i]}")
