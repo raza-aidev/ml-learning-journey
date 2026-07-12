9690ba1aaa2ee76bcf568cf0f6a3ef8ca3cb9988
raza-aidev <raza.aidev@gmail.com>
2026-07-12 17:59:55 +0530

    Addding list accessing exmaples in python

---
+++ phase1/python-list/list-accessing.py
@@ -0,0 +1,7 @@
+list1 = ["a", 'b', "c", 12, 13.5, "letters", 'you']
+str1 = "letters"
+print("accesing every thrid element of a list: ", list1[::3])
+
+print(f"Origination list is {list1}")
+print(f"reversed list is {list1[::-1]}")
+print(f"reversed pf the string {str1} is {str1[::-1]}")
