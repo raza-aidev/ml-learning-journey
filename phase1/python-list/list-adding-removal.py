1a619cc7608d98d0050a8c9c42dd87871da5a310
raza-aidev <raza.aidev@gmail.com>
2026-07-13 20:20:35 +0530

    Adding more examples

--- phase1/python-list/list-adding-removal.py
+++ phase1/python-list/list-adding-removal.py
@@ -38,15 +38,34 @@ list3 = list2[:]
 list4 = list1[:]
 print(f"New List: {list3}")
 list3.clear()
-print(f"List After clearing: {list3}; gettuing Empty list")
+print(f"List After clearing: {list3}; getting Empty list")
 # del list4
 # print(f"list after deleteion: {list4}") # Going to get error
-list_new = []
-while len(list_new) != 10:
-    list_new.append(input("Enter the string:"))
-print(f"Your final list is: {list_new}")
-for data in list_new:
-    print(data, end=" ")
+# list_new = []
+# while len(list_new) != 10:
+#     list_new.append(input("Enter the string:"))
+# print(f"Your final list is: {list_new}")
+# for data in list_new:
+#     print(data, end=" ")
 
-print()
-    
+# print()
+# count = 0;
+# print(reversed(list1))
+# list5 = reversed(list1)
+# print(list5)
+# # for item in list1:
+# #     print(item)
+
+# # while count < len(list1):
+# #     print(f"Reversed List: {list1[count]}")
+# #     count+=1;
+
+print("iterating hrough 2 lists: ")
+for item1, item2 in zip(list1, list2):
+    print(f"List1 string: {item1} list2 String: {item2}")
+
+list_2d = [[12, 13, 14],["Ask",'your',"manager", "for", "feedback"],['a','b','c','d','e'],[12.3, 13.6,11.6,113.3]]
+for _ in list_2d:
+    print(f"Printing list: {_}")
+    for index, item in enumerate(_):
+        print("Item at {}: {}".format(index,item))
