b71e3ee58e24a50f93bef993856cb6ffb005612f
raza-aidev <raza.aidev@gmail.com>
2026-07-15 23:40:45 +0530

    adding tuple examples

--- phase1/python-tuple/tuple.py
+++ phase1/python-tuple/tuple.py
@@ -19,8 +19,27 @@ print(f"type of tuple2: {type(tuple2)}, Type of tuple3: {type(tuple3)}, type of 
 tuple1 = list(tuple1)
 list2 = [12, 13, 14]
 
-tuple1 = [tuple1+i for i in list2]
-print("new tuple: ", tuple1)
+# tuple1 = [tuple1+i for i in list2]
+# print("new tuple: ", tuple1)
 
-tuple1 = tuple(tuple1)
-print("updated tuple: ",tuple1)
+# tuple1 = tuple(tuple1)
+# print("updated tuple: ",tuple1)
+
+
+tuple2 = (12, 13.5, 'test', "ABD", 'a')
+a, b, c, *d = tuple2
+
+print(f"a: {a}, b: {b}, c: {c}, d: {d}")
+
+
+print("-----------looping through tuple---------")
+#using index and for loop
+for i in range(len(tuple2)):
+    print(f"Index {i}: {tuple2[i]}")
+
+#using while loop
+count = 0
+print("Looping tuple using while -> ")
+while count< len(tuple2):
+    print(f"{tuple2[count]}")
+    count +=1
