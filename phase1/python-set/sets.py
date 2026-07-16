60940c555d5d6fc5f774de67b604af691f8db32f
raza-aidev <raza.aidev@gmail.com>
2026-07-16 23:08:08 +0530

    adding set comprehension exmples

--- phase1/python-set/sets.py
+++ phase1/python-set/sets.py
@@ -18,10 +18,22 @@ for i in temp_set:
     
 print("New Set1: {}".format(set1))
 
-temp_set = {23, "raza", 'r', 1.1}
-for obj in temp_set:
-    temp_set.remove(obj)
+# temp_set = {23, "raza", 'r', 1.1}
+# for obj in temp_set:
+#     temp_set.remove(obj)
 
 # temp_set.discard();
 print("New temp set: ",temp_set)
+
+#set comprehension 
+
+set1 = {obj**2 for obj in range(14)}
+print(f'Set using list comprehension: {set1}')
+
+# nested set comprehension
+set2 = {(obj1, obj2) for obj1 in range(5) for obj2 in range(6)}
+print(f"nested if else: {set2}")
+
+
+
 
