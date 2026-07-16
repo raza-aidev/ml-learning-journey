82229b494d613fd203465d8f74bb3778a8fa979f
raza-aidev <raza.aidev@gmail.com>
2026-07-16 22:59:22 +0530

    adding set examples

---
+++ phase1/python-set/sets.py
@@ -0,0 +1,27 @@
+set1 = {1, 2, 3, 4}
+print("Set: {}".format(set1))
+
+# set can't have duplicate objects
+
+set2 = {1, 2, 1, 4, 5, 6, 2}
+print("No Duplicate objects: {}".format(set2))
+
+# Set is mutable, un-ordered, mutable object
+# Adding new objects in set
+
+set1.add(84)
+print("New Set1: ",set1)
+
+temp_set = {23, "raza", 'r', 1.1}
+for i in temp_set:
+    set1.add(i)
+    
+print("New Set1: {}".format(set1))
+
+temp_set = {23, "raza", 'r', 1.1}
+for obj in temp_set:
+    temp_set.remove(obj)
+
+# temp_set.discard();
+print("New temp set: ",temp_set)
+
