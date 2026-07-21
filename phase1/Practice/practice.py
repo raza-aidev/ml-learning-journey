00f1575f2fd48e5918fd24a81f2fe15a259f7b5e
raza-aidev <raza.aidev@gmail.com>
2026-07-21 22:42:53 +0530

    adding example #1 in file

---
+++ phase1/Practice/practice.py
@@ -0,0 +1,28 @@
+"""
+Given an integer, n , perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+
+"""
+# n = int(input("Enter Number: "))
+
+# if (n%2 != 0):
+#     print("Weird")
+# else:
+#     if (2 <= n <= 5):
+#         print("Not Weird")
+#     elif(6 <= n <=20):
+#         print("Weird")
+#     elif(n > 20):
+#         print("Not Weird") 
+
+"""
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+"""
