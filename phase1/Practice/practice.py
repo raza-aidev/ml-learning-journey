a5e10b700b7d0c7b2dfa099287f08345d4759f4a
raza-aidev <raza.aidev@gmail.com>
2026-07-21 22:50:01 +0530

    Adding example #2

--- phase1/Practice/practice.py
+++ phase1/Practice/practice.py
@@ -20,9 +20,28 @@ If n is even and greater than 20, print Not Weird
 #         print("Not Weird") 
 
 """
-The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
 
 The first line contains the sum of the two numbers.
 The second line contains the difference of the two numbers (first - second).
 The third line contains the product of the two numbers.
+
 """
+
+a, b = int(input("Enter Value of a: ")), int(input("Enter Value of b: "))
+
+print(a+b)
+print(a-b)
+print(a*b)
+
+
+"""
+The provided code stub reads two integers, a and b, from STDIN.
+
+Add logic to print two lines. The first line should contain the result of integer division, a // b. 
+The second line should contain the result of float division,  / .
+
+No rounding or formatting is necessary.
+
+"""
+
