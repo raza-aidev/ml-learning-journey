c5e915ab4c122a002856f8c9cf016e7bf0c61fc4
raza-aidev <raza.aidev@gmail.com>
2026-07-22 23:42:43 +0530

    Adding New Examples of OOP

--- phase1/OOP/class1.py
+++ phase1/OOP/class1.py
@@ -1,11 +1,50 @@
 class Dog:
-    def __init__(self, name, breed, age):
+
+    breed = "cane"
+    purpose = "Gaurd Dog"
+
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.breed = breed
-        print(f"Woof! it's {name} and i'm {age} years old. And I'm {breed} dog... Woof! Woof!")
+        
+        # print(f'I am a {Dog.purpose}.')
 
-d1 = Dog('Max', 'labrador', '4')
-d2 = Dog('Lucy', 'Germen S', '15')
+    def bark(self):
+        return f"Woof! it's {self.name} and i'm {self.age} years old. And I'm {Dog.breed} dog... Woof! Woof!"
+    
+    def run(self):
+        return f"I'm {self.name}....weee!!! i'm running behind vehicles"
 
+d1 = Dog('Max', '4')
+d2 = Dog('Lucy', '15')
+
+print(d1.bark())
+print(d1.run())
+
+print(d2.bark())
+print(d2.run())
 # d1.Dog(f"{d1.}")
+
+
+"""
+Inside the editor, complete the following steps:
+Create a class called Person
+Add an __init__ method that takes name and age as parameters
+Add a method called greet that prints "Hello, my name is " followed by the name
+Create an object p1 of the class with name "John" and age 36
+Call the greet method on p1
+
+"""
+
+class Person:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, my name is {self.name} and age is {self.age}")
+    
+p1 = Person("Jhon", 36)
+
+p1.greet()
