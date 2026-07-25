d1d77ee0ed9c25d581891de23175caf9cabc3d43
raza-aidev <raza.aidev@gmail.com>
2026-07-25 23:58:08 +0530

    Adding examples from Hacker rank

--- phase1/Practice/practice.py
+++ phase1/Practice/practice.py
@@ -45,7 +45,52 @@ No rounding or formatting is necessary.
 
 """
 
-a , b = int(input("Enter Value of a: ")), int(input("Enter Value of b: "))
+# a , b = int(input("Enter Value of a: ")), int(input("Enter Value of b: "))
 
-print(a//b)
-print(a/b)
+# print(a//b)
+# print(a/b)
+
+
+"""
+Task
+The provided code stub reads an integer, n, from STDIN. For all non-negative integers ,i<n print i2.
+
+Example
+
+"""
+
+# n = int(input("Enter integer number: "))
+
+# for i in range(n):
+#     if i > 0:
+#         print(i*i)
+
+
+"""
+
+An extra day is added to the calendar almost every four years as February 29, and the day is called a leap day. It corrects the calendar for the fact that our planet takes approximately 365.25 days to orbit the sun. A leap year contains a leap day.
+
+In the Gregorian calendar, three conditions are used to identify leap years:
+
+The year can be evenly divided by 4, is a leap year, unless:
+The year can be evenly divided by 100, it is NOT a leap year, unless:
+The year is also evenly divisible by 400. Then it is a leap year.
+This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years. Source
+
+Task
+
+Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
+
+Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
+
+"""
+
+def is_leap(year):
+    if(year%4 == 0 and year%100 != 0 or year%400 == 0):
+        return True
+    else:
+        return False
+
+year = int(input("Enter the year: "))
+
+print(is_leap(year))
