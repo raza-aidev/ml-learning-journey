77173f0200266161045909705052f2e7ad99139e
raza-aidev <raza.aidev@gmail.com>
2026-07-27 23:28:07 +0530

    Adding dunder methods __str__(), __eq__(), __lt__(), __gt__(), __contains__(), __getItems__()

---
+++ phase1/OOP/dunder-methodes.py
@@ -0,0 +1,54 @@
+
+
+class Book:
+    def __init__(self, title, author, num_pages):
+        self.title = title
+        self.author = author
+        self.num_pages = num_pages
+    
+    def __str__(self):
+        """ This method represents class object in string format"""
+        return f"Title: {self.title}, Author: {self.author}, num_pages: {self.num_pages}"
+    
+    def __eq__(self, other):
+        """ This method validate if 2 objects are equal or not"""
+        if self.title == other.title and self.author == other.author:
+            return True
+        else:
+            return False
+    
+    def __lt__(self, other):
+        return self.num_pages < other.num_pages
+
+    def __gt__(self, other):
+        return self.num_pages > other.num_pages
+    
+    def __contains__(self, keyword):
+        return keyword in self.title
+
+    def __getItems__(self, key):
+        return s
+
+
+
+book1 = Book("Harry Potter and the Philosofer stone", "J.K Rowling", 555)
+book2 = Book("The Hobbit", "R.R Tokien", 456)
+book3 = Book("Bad Habbits", "V.R Vishnu", 146)
+# book4 = Book("The Hobbit", "R.R Tokien", 116)
+
+
+# print(book1)
+
+# print(book4 == book2) # True
+# print(book1 == book2) # False
+
+# print(book1 < book2)
+# print(book1 > book2)
+
+print("Bad" in book1)
+print("Bad" in book2)
+print("Bad" in book3)
+
+
+
+
