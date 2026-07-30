e6ac49439cf9bcdb043994fe1fe8601895c38336
raza-aidev <raza.aidev@gmail.com>
2026-07-30 08:02:48 +0530

    Understanding and solving python aggregation problems

---
+++ phase1/OOP/week-five-aggregation.py
@@ -0,0 +1,12 @@
+class Library:
+    def __init__(self, lib_name):
+        self.lib_name = lib_name
+        self.book = []
+
+    def list_of_books(self, ) 
+
+class Books:
+    def __init__(self, book_name, author):
+        self.book_name = book_name
+        self.author - author
+    
