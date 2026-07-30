6438b30a195903e7d2b7c142045cb5fcf028e58c
raza-aidev <raza.aidev@gmail.com>
2026-07-30 08:45:35 +0530

    Adding example of aggregation

--- phase1/OOP/week-five-aggregation.py
+++ phase1/OOP/week-five-aggregation.py
@@ -1,12 +1,30 @@
 class Library:
     def __init__(self, lib_name):
         self.lib_name = lib_name
-        self.book = []
+        self.books = []
 
-    def list_of_books(self, ) 
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_of_books(self):
+        return [f"{book.book_name} and {book.author}" for book in self.books] # here we are accessing list of books represented as "self.books", the list of books contains book objects containing author and name properties of books objects pass to the library.
 
 class Books:
     def __init__(self, book_name, author):
         self.book_name = book_name
-        self.author - author
+        self.author = author
     
+lib = Library("Deccan Library")
+
+book1 = Books("Harry Potter", "J.K Rowling")
+book2 = Books("White House in the dark woods", "A.R Kafil")
+book3 = Books("Lake of Pearls", "R.Rajkumar")
+
+lib.add_book(book1)
+lib.add_book(book2)
+lib.add_book(book3)
+
+# print(list(lib.list_of_books()))
+
+for book in lib.list_of_books():
+    print(book)
