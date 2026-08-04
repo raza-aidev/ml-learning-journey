2c4e31003590e9c50fdadd1feea6cc8d0d836652
raza-aidev <raza.aidev@gmail.com>
2026-08-04 07:57:18 +0530

    adding examples of map functions

---
+++ phase1/Functions/week-six-map.py
@@ -0,0 +1,63 @@
+"""
+Create Square all numbers
+
+numbers = [2, 4, 6, 8, 10]
+
+"""
+numbers = [2, 4, 6, 8, 10]
+squares = list(map(lambda num: num**2, numbers))
+
+print(squares)
+
+
+"""
+Question 2 - Convert to Uppercase
+names = ["raza", "john", "alex", "maria"]
+"""
+
+names = ["raza", "jhon", "alex", "maria"]
+
+def to_upper(*names):
+    
+    for name in names:
+        return str(name.upper())
+    
+
+upper_case = list(map(to_upper, names))
+
+print(upper_case)
+
+"""
+words = ["Python", "Java", "C++", "JavaScript"]
+Question 3 - Find Length of Every Word
+"""
+words = ["Python", "Java", "C++", "JavaScript"]
+def find_length(*words):
+    for word in words:
+        return len(word)
+
+word_length = list(map(find_length, words))
+
+print(word_length)
+
+
+"""
+Add 18% GST to every price.
+prices = [100, 250, 500, 1000]
+"""
+prices = [100, 250, 500, 1000]
+
+GST = list(map(lambda num: (num*0.18)+num, prices))
+
+print(GST)
+
+"""
+Convert Celsius to Fahrenheit
+celsius = (temps * 9/5)+32
+temps = [0, 10, 20, 30, 40]
+
+"""
+temps = [0, 10, 20, 30, 40]
+temps_celsius = list(map(lambda temp: (temp*9/5)+32 , temps))
+
+print(temps_celsius)
