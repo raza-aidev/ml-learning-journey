0f65b0b7f37e4ccd4c6bf1ecfc5994b2e5ec5181
raza-aidev <raza.aidev@gmail.com>
2026-08-04 22:41:34 +0530

    adding examples of ma function

--- phase1/Functions/week-six-map.py
+++ phase1/Functions/week-six-map.py
@@ -61,3 +61,38 @@ temps = [0, 10, 20, 30, 40]
 temps_celsius = list(map(lambda temp: (temp*9/5)+32 , temps))
 
 print(temps_celsius)
+
+"""
+Keep only even numbers.
+numbers = [5,8,12,17,19,20,25]
+Question 6 - Even Numbers
+"""
+numbers = [5,8,12,17,19,20,25]
+
+def is_even(*nums):
+    for num in nums:
+        if num%2 == 0:
+            return num
+    
+even_nums = list(filter(is_even, numbers))
+
+print(even_nums)
+
+"""
+Return only words having more than 5 letters.
+words = ["cat","elephant","dog","python","sun"]
+
+"""
+words = ["cat","elephant","dog","python","sun"]
+
+def check_len(*words):
+    # for word in words:
+    #     result = ""
+    #     if len(word)>=5:
+    #         return word
+    # print(words)
+        
+check_len(words)
+
+Max_length = list(map(check_len , words))
+# print(Max_length)
