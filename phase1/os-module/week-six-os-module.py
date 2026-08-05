cac7ab7f00ec446c7719c9f2925926c8990b8f99
raza-aidev <raza.aidev@gmail.com>
2026-08-05 23:58:34 +0530

    adding OS module notes and and few questions

--- phase1/os-module/week-six-os-module.py
+++ phase1/os-module/week-six-os-module.py
@@ -37,9 +37,32 @@ print(os.listdir("."))
 
 # print(os.listdir())
 # os.mkdir("parent2")
-if(os.path.exists("test2.txt")):
-    os.rename("week-six-os-module.py", "/parent2/week-six-os-module.py")
-else:
-    os.mkdir("test2.txt")
+# if(os.path.exists("test2.txt")):
+#     os.rename("week-six-os-module.py", "/parent2/week-six-os-module.py")
+# else:
+#     os.mkdir("test2.txt")
 
-print(os.listdir("./parent1"))
+# print(os.listdir("./parent1"))
+
+# print(f"{os.getcwd()}")
+path = "/home/abdulrazzaque/Road-to-AIDev/ml-learning-journey/phase1/os-module/week-six-os-module.py"
+abspath = "test.py"
+abspath2 = "test.tft.py"
+dumm_path = "C:/group/Blood"
+file = "B_Pos.txt"
+print(f"{os.path.dirname(path)}")
+print(f"{os.path.basename(path)}")
+print(f"{os.path.split(path)}")
+print(f"{os.path.splitext(path)}")
+print(f"{os.path.splitext(abspath)}")
+print(f"{os.path.join(dumm_path, file)}")
+
+print(f"{os.path.abspath(path)}")
+print(f"{os.path.isabs(dumm_path)}")
+print(f"{os.path.isabs(file)}")
+print(f"{os.path.isabs(path)}")
+
+
+
+
+
