9ded87c04d50616859eb3e2d0e5e22f9815421f6
raza-aidev <raza.aidev@gmail.com>
2026-08-06 22:27:51 +0530

    adding new examples of os-module

--- phase1/os-module/week-six-os-module.py
+++ phase1/os-module/week-six-os-module.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 
 print(os.getcwd())
 # os.chdir("/home/abdulrazzaque/Road-to-AIDev/ml-learning-journey/phase1/OOP/week-fiv-abstraction.py")    
@@ -62,7 +62,34 @@ print(f"{os.path.isabs(dumm_path)}")
 print(f"{os.path.isabs(file)}")
 print(f"{os.path.isabs(path)}")
 
+modi_time = os.path.getmtime(path)
+print(f"Modification time: {time.ctime(modi_time)}")
+
+creation_time = os.path.getctime(path)
+print(f"Creation time: {time.ctime(creation_time)}")
+
+access_time = os.path.getatime(path)
+print(f"Access time: {time.ctime(access_time)}")
+
+# print(os.environ)
+
+# print(f'{os.getenv(path)}')
+
+print(tuple(os.walk('.')))
 
 
+txt_files = [f for r, d, files in os.walk(".") for f in files if os.path.isfile(f)]
 
+print("Text files: {}".format(txt_files))
 
+# for root, dirs, files in os.walk("ml-learning-journey"):
+#     print(f"Root: {root}")
+#     print(f"Directories: {dirs}")
+#     print(f"Files: {files}")
+
+print(os.path.isdir("/home/abdulrazzaque/Road-to-AIDev/ml-learning-journey/phase1/os-module/test2.txt"))
+
+# os.mkdir("textfile.txt")
+os.mkfifo("textfile1.txt")
+print(os.path.isfile("textfile1.txt"))
+os.remove("textfile1.txt")
