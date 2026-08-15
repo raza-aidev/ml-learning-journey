d52cd2afa4447d0b492f7ef75711c870afdea07f
raza-aidev <raza.aidev@gmail.com>
2026-08-15 15:16:38 +0530

    Python logging understanding notes

--- phase1/python-logging/python-logging.py
+++ phase1/python-logging/python-logging.py
@@ -11,13 +11,22 @@
 # # logging.error("This is the error")
 # # logging.critical("This is the critical message!")
 
-import logging
+# import logging
 
-logging.basicConfig(level = logging.INFO, 
-                    format = "%(asctime)s - %(levelno)d - %(name)s", 
-                    filename = "test-log.log", 
-                    filemode = "a")
+# logging.basicConfig(level = logging.DEBUG, 
+#                     datefmt = '%d:%m:%Y %H:%M:%S',
+#                     format = " %(asctime)s | %(name)s:%(lineno)d | %(levelname)s : %(message)s", 
+#                     filename = "test-log.log", 
+#                     filemode = "a")
 
-logger = logging.getLogger("python-logging")
-logger.setLevel(logging.INFO)
-logger.info("This is the message for info")
+# # logger = logging.getLogger("python-logging")
+# logger = logging.getLogger("python-logging")
+
+
+# logger.setLevel(logging.DEBUG)
+# logger.debug("This is the message for info")
+# logger.error("This is the error")
+# logger.info("This is the message of information ")
+# logger.warning("This is the Warning")
+# logger.critical("This is critile message")
+
