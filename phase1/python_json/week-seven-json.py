2517685920d24d810fffaadf279a859563e0d83b
raza-aidev <raza.aidev@gmail.com>
2026-08-19 06:46:39 +0530

    Adding emaples of python data tp json cnversion

---
+++ phase1/python_json/week-seven-json.py
@@ -0,0 +1,62 @@
+import json
+import logging
+
+# #create logger
+# logger = logging.getLogger("week-seven")
+# logger.setLevel(logging.INFO)
+
+# #create handler
+# console = logging.StreamHandler()
+# console.setLevel(logging.INFO)
+
+# #add handler to logger
+# logger.addHandler(console)
+
+# python_string = {
+#     "name":"raza",
+#     "age":30,
+#     "Married":True
+#     }
+
+# #converting to JSON string
+# json_string = json.dumps(python_string)
+
+# logger.info(type(json_string))
+# logger.info(json_string)
+
+
+# python_dictionary = """{"std_name": "Rakesh", "marks": [55, 78, 89], "class": "10th", "division": "A", "Pass": true}"""
+
+# json_value = json.loads(python_dictionary)
+# logger.info(json_value)
+# logger.info(type(json_value))
+
+#send data to API
+
+python_data = {
+    "action":"create-user",
+    "user-details":{
+        "user-name":"Ravi Shastri",
+        "age":"67",
+        "profession":"Cricket Coach",
+        "centuries": 101
+    },
+    "stats": [12, 32, 56]
+}
+
+#create logger
+logger = logging.getLogger("week-seven-json")
+logger.setLevel(logging.DEBUG)
+
+#create handler
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+
+#add handler to logger
+logger.addHandler(console)
+
+#Convert python data to JSON (for API)
+json_data=json.dumps(python_data)
+logger.debug(f"the JSON data: {json_data}")
+
+logger.debug(f"Type of Data: {type(json_data)}")
