75805a07fae14e306ab576b26ad3bf85ecd815f0
raza-aidev <raza.aidev@gmail.com>
2026-08-24 09:14:34 +0530

    Adding examples of walurus operator for practice

---
+++ phase1/python_operators/week-eight-walrus-prac.py
@@ -0,0 +1,52 @@
+import logging
+
+logger = logging.getLogger("week-eight-walrus-prac")
+logger.setLevel(logging.DEBUG)
+
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+
+logger.addHandler(console)
+
+"""
+Problem 4 — While loop
+
+Use a while loop and walrus operator to continuously accept numbers.
+
+Stop when the user enters -1.
+
+Example:
+
+Enter number: 10
+You entered 10
+
+
+Enter number: 20
+You entered 20
+
+
+Enter number: 5
+You entered 5
+
+
+Enter number: -1
+Program ended
+"""
+
+while (num := int(input("Enter Number: "))) != -1:
+    logger.info(f"The entered number is: {num}")
+
+
+"""
+Problem 5 — Find first long word
+
+Given:
+
+words = ["cat", "dog", "elephant", "fox", "tiger"]
+
+Using the walrus operator, find the first word whose length is greater than 5.
+
+Expected:
+
+elephant
+"""
