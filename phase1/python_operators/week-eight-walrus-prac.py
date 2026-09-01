942e9103dc396b9cf66ed38c19c78ddb61c94a7e
raza-aidev <raza.aidev@gmail.com>
2026-09-01 09:14:20 +0530

    Adding walrus operator examples

--- phase1/python_operators/week-eight-walrus-prac.py
+++ phase1/python_operators/week-eight-walrus-prac.py
@@ -68,3 +68,45 @@ for i in range(len(words)):
     if len((word := words[i])) > 5:
         print(word)
         break
+
+"""
+numbers = [10, 20, 30, 40, 50]
+Use the walrus operator to calculate the sum and print:
+
+Sum is greater than 100: 150
+
+if the sum is greater than 100.
+"""
+ 
+numbers = [10, 20, 30, 40, 50]
+
+if (sum_of_num := sum(numbers)) >= 100:
+    logger.info(f"Sum of Number: {sum_of_num}")
+
+
+"""
+Problem 4 — While loop
+
+Use a while loop and walrus operator to continuously accept numbers.
+
+Stop when the user enters -1.
+
+Example:
+
+Enter number: 10
+You entered 10
+
+Enter number: 20
+You entered 20
+
+Enter number: 5
+You entered 5
+
+Enter number: -1
+Program ended
+
+"""
+
+while (num := int(input("Enter the number: "))) != -1:
+    logger.info(f"The entered number is: {num}")
+
