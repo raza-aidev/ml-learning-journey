dbec093eed36ad2fc47ea1311bf91b703acb16c5
raza-aidev <raza.aidev@gmail.com>
2026-09-08 22:52:57 +0530

    Examples of lis comprehension

---
+++ phase1/python/python-practice/week-ten-comprehension.py
@@ -0,0 +1,27 @@
+import logging 
+
+logger = logging.getLogger("week-nine-comprehension")
+logger.setLevel(logging.DEBUG) # DEBUG, INFO, WARNING, ERROR, CRITICAL
+
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+
+logger.addHandler(console)
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+even_nums = [even_num for even_num in list1 if (even_num%2) == 0]
+logger.info(even_nums)
+
+list2 = ["hello", "How", "are", 'You!']
+
+upper_case = [word.upper() for word in list2]
+logger.info(upper_case)
+
+list3 = ["hello", "Apple", "Banan", "Ptato", "Tomato"]
+word_lens = [len(word) for word in list3]
+logger.info(word_lens)
+
+list4 = [-1, -2, -3, -0.5, 1, 3, 4, -2, 0]
+neg_nums = [num if num > 0 else 0 for num in list4 ]
+logger.info(neg_nums)
