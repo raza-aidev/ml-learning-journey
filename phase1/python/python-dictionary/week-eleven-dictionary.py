3da8a215a9541a2f0b2ec3fbc607bc1a4543e772
raza-aidev <raza.aidev@gmail.com>
2026-09-19 11:04:58 +0530

    Adding more examples of python dictionary moification

--- phase1/python/python-dictionary/week-eleven-dictionary.py
+++ phase1/python/python-dictionary/week-eleven-dictionary.py
@@ -1,3 +1,13 @@
+import logging
+
+logger = logging.getLogger('week-eleven-dictionary')
+logger.setLevel(logging.INFO)
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+
+logger.addHandler(console)
+
 dict1 = {'a': 2, 'B':"Harry", 'c':'Qali'}
 
 dict1['a']=3
@@ -20,3 +30,23 @@ d = dict.fromkeys(["k",'l'], [])
 print(d)
 d['k'].append("3")
 print(d)
+
+d1 = {'age': 14, "name": 'Sarah', 'height': "5 4'"}
+
+logger.info(d1.get('age'))
+logger.error(d1.get('NAME')) # DEBUG, INFO, WARNING, ERROR, CRITICAL
+
+logger.error(f"Not present: {d1.get('NAME')}")
+
+# logger.error(f"{d1['occupation']}")
+
+
+#Adding data in dictionary examples
+
+details = {"Age":15, "Name": "Ali", "Occupation":"10th Std(Studying)", "School_Name": "Trinity School", "Weight":56}
+
+logger.info(f"Original Dict: {details}")
+
+details["Surname"] = "Khan"
+
+logger.info(f"Updated dictionary: {details} ")
