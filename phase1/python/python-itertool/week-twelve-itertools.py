d172633c6792907ccb87e05239ac38433de6e19f
raza-aidev <raza.aidev@gmail.com>
2026-09-21 23:59:43 +0530

    examples of count(), cycle(), repeat()

--- phase1/python/python-itertool/week-twelve-itertools.py
+++ phase1/python/python-itertool/week-twelve-itertools.py
@@ -1,4 +1,4 @@
-from itertools import count, islice, cycle, repeat 
+from itertools import count, islice, cycle, repeat, chain
 import logging
 
 logger = logging.getLogger("week-twelve-itertools")
@@ -19,3 +19,13 @@ logger.info(f'Use of cycle function: {list(islice(cycle(color), 8))}')
 
 # repeat 
 logger.info(f'User of repeat: {list(repeat(12, 15))}')
+
+# chain 
+use_of_chain = list(chain((1, 2), [3, 4], range(5, 11), {11, 12, 13}))
+logger.info(f'Use of chain function: {use_of_chain}')
+
+# islice
+list1 = list(islice(range(0, 11), 5))
+logger.info(list1)
+
+list2 = list(islice())
