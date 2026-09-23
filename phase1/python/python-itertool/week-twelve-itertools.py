918d722db892d168b53cb991a9b855e3b0c605d0
raza-aidev <raza.aidev@gmail.com>
2026-09-23 07:39:15 +0530

    Examples of reduce functions

--- phase1/python/python-itertool/week-twelve-itertools.py
+++ phase1/python/python-itertool/week-twelve-itertools.py
@@ -43,3 +43,23 @@ logger.debug(output2)
 
 output3 = list(itertools.accumulate("abc", lambda x, y: x*3 + y*2))
 logger.debug(output3)
+
+import operator
+
+output4 = list(itertools.accumulate(numbers, operator.mul))
+logger.debug(output4)
+
+output5 = list(itertools.accumulate(numbers, operator.add))
+logger.debug(output5)
+
+output6 = list(itertools.accumulate(numbers, operator.sub))
+logger.debug(output6)
+
+numbers2 = [3, 1, 4, 5, 1, 1, 7, 1]
+output7 = list(itertools.accumulate(numbers2, max))
+logger.debug(f"max: {output7}")
+
+from functools import reduce
+
+output8 = reduce(lambda x,y: x*y ,numbers2)
+logger.debug(output8)
